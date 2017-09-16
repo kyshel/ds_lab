bd4621925551232f145fcb0a559aadc7f7a06cc3
kyshel <1069072177@qq.com>
2017-09-16 12:31:35 +0800

    aa

---
+++ src_hi.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void printTree();
+void test();
+
+int main(){
+	//printTree();
+	test();
+	return 0;
+}
+
+void printTree(){
+	int index = 0; 
+ 
+	for (int i = 1; i < 20; i++)
+	{
+		if (i > pow(2,index))
+		{
+			cout << endl ;
+			index++;
+		}
+
+		cout << i << " ";
+	}
+
+	cout << "\n";
+} 
+
+void test(){
+	int p ;
+	p= 10;
+
+	cout << "p is " << p << endl;
+	//cout << "*p is " << *p;
+	cout << "&p is " << &p << endl;
+}
